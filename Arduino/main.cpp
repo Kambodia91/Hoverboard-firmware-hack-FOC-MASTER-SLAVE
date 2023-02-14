40a86855e5110f82eaf551426c2d74941849df38
Kambodia91 <jasiucorp@gmail.com>
2023-02-14 02:29:51 +0100

    c

--- Arduino/main.cpp
+++ Arduino/main.cpp
@@ -14,10 +14,10 @@
 // The code starts with zero speed and moves towards +
 //
 // CONFIGURATION on the hoverboard side in config.h:
-// • Option 1: Serial on Right Sensor cable (short wired cable) - recommended, since the USART3 pins are 5V tolerant.
-//   #define CONTROL_SERIAL_USART3
-//   #define FEEDBACK_SERIAL_USART3
-//   // #define DEBUG_SERIAL_USART3
+// • Option 1: Serial on Right Sensor cable (short wired cable) - recommended, since the USART1 pins are 5V tolerant.
+//   #define CONTROL_SERIAL_USART1
+//   #define FEEDBACK_SERIAL_USART1
+//   // #define DEBUG_SERIAL_USART1
 // • Option 2: Serial on Left Sensor cable (long wired cable) - use only with 3.3V devices! The USART2 pins are not 5V tolerant!
 //   #define CONTROL_SERIAL_USART2
 //   #define FEEDBACK_SERIAL_USART2
@@ -25,17 +25,14 @@
 // *******************************************************************
 
 // ########################## DEFINES ##########################
-#define HOVER_SERIAL_BAUD   115200      // [-] Baud rate for HoverSerial (used to communicate with the hoverboard)
+#define HOVER_SERIAL_BAUD   115200      // [-] Baud rate for Serial2 (used to communicate with the hoverboard)
 #define SERIAL_BAUD         115200      // [-] Baud rate for built-in Serial (used for the Serial Monitor)
 #define START_FRAME         0xABCD     	// [-] Start frme definition for reliable serial communication
-#define TIME_SEND           100         // [ms] Sending time interval
-#define SPEED_MAX_TEST      300         // [-] Maximum speed for testing
-#define SPEED_STEP          20          // [-] Speed step
-// #define DEBUG_RX                        // [-] Debug received data. Prints all bytes to serial (comment-out to disable)
-
-#include <SoftwareSerial.h>
-SoftwareSerial HoverSerial(2,3);        // RX, TX
-
+#define TIME_SEND           20         // [ms] Sending time interval
+#define SPEED_MAX_TEST      100         // [-] Maximum speed for testing
+#define SPEED_STEP          1          // [-] Speed step
+//#define DEBUG_RX                        // [-] Debug received data. Prints all bytes to serial (comment-out to disable)
+#include <Arduino.h>
 // Global variables
 uint8_t idx = 0;                        // Index for new data pointer
 uint16_t bufStartFrame;                 // Buffer Start Frame
@@ -71,7 +68,7 @@ void setup()
   Serial.begin(SERIAL_BAUD);
   Serial.println("Hoverboard Serial v1.0");
 
-  HoverSerial.begin(HOVER_SERIAL_BAUD);
+  Serial2.begin(HOVER_SERIAL_BAUD);
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
@@ -85,15 +82,15 @@ void Send(int16_t uSteer, int16_t uSpeed)
   Command.checksum = (uint16_t)(Command.start ^ Command.steer ^ Command.speed);
 
   // Write to Serial
-  HoverSerial.write((uint8_t *) &Command, sizeof(Command)); 
+  Serial2.write((uint8_t *) &Command, sizeof(Command)); 
 }
 
 // ########################## RECEIVE ##########################
 void Receive()
 {
     // Check for new data availability in the Serial buffer
-    if (HoverSerial.available()) {
-        incomingByte 	  = HoverSerial.read();                                   // Read the incoming byte
+    if (Serial2.available()) {
+        incomingByte 	  = Serial2.read();                                   // Read the incoming byte
         bufStartFrame	= ((uint16_t)(incomingByte) << 8) | incomingBytePrev;       // Construct the start frame
     }
     else {
@@ -162,7 +159,7 @@ void loop(void)
   // Send commands
   if (iTimeSend > timeNow) return;
   iTimeSend = timeNow + TIME_SEND;
-  Send(0, iTest);
+  Send(iTest, iTest);
 
   // Calculate test command signal
   iTest += iStep;
